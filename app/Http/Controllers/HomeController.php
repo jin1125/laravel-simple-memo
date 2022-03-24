5adba23fd875096b762146cdeadf8ae60d48fcbb
jin1125 <shogo.jinta@gmail.com>
2022-03-24 10:27:46 +0900

    31

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Memo;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,23 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $memos = Memo::select('memos.*')
+        ->where('user_id', '=', \Auth::id())
+        ->whereNull('deleted_at')
+        ->orderBy('updated_at', 'DESC')
+        ->get();
+        dd($memos);
+
         return view('create');
+    }
+
+    public function store(Request $request)
+    {
+        $posts = $request->all();
+        Memo::insert([
+            'content' =>  $posts['content'],
+            'user_id' => \Auth::id()
+        ]);
+        return redirect(route('home'));
     }
 }
