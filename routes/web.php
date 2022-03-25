b21aa74780cf8290baff24f6d2b91b2869775819
jin1125 <shogo.jinta@gmail.com>
2022-03-25 22:22:35 +0900

    fix

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,11 @@ use App\Http\Controllers\HomeController;
 |
 */
 
+
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('index');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::post('/store', [HomeController::class, 'store'])->name('store');
 Route::get('/edit/{id}', [HomeController::class, 'edit'])->name('edit');
 Route::post('/update', [HomeController::class, 'update'])->name('update');
